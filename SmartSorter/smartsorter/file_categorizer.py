d68efb0f327ecac452b5a2c3c5c26fb8e8dde9df
HimashaHerath <himasha626@gmail.com>
2023-11-22 19:23:08 +0530

    Add attribute-based rules for file categorization

--- SmartSorter/smartsorter/file_categorizer.py
+++ SmartSorter/smartsorter/file_categorizer.py
@@ -1,3 +1,5 @@
+import time
+
 def categorize_files(files, rules):
     """
     Categorizes files based on provided rules.
@@ -15,17 +17,31 @@ def categorize_files(files, rules):
         folders = directory.split(os.sep)
         extension = filename.split('.')[-1]
 
-        # Check for folder-based rules
-        for folder in folders:
-            if folder in rules:
-                category = rules[folder]
-                break
+        # Get file attributes
+        stat = os.stat(file)
+        creation_date = time.ctime(stat.st_ctime)
+        last_modified_date = time.ctime(stat.st_mtime)
+        file_size = stat.st_size
+
+        # Check for attribute-based rules
+        if 'creation_date' in rules and creation_date == rules['creation_date']:
+            category = 'creation_date'
+        elif 'last_modified_date' in rules and last_modified_date == rules['last_modified_date']:
+            category = 'last_modified_date'
+        elif 'file_size' in rules and file_size == rules['file_size']:
+            category = 'file_size'
         else:
-            # If no folder-based rule is found, use extension-based rule
-            if extension in rules:
-                category = rules[extension]
+            # If no attribute-based rule is found, check for folder-based rules
+            for folder in folders:
+                if folder in rules:
+                    category = rules[folder]
+                    break
             else:
-                continue
+                # If no folder-based rule is found, use extension-based rule
+                if extension in rules:
+                    category = rules[extension]
+                else:
+                    continue
 
         if category not in categorized:
             categorized[category] = []
